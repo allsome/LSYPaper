cdfc7605c34a3501dec3cdf32aaf09f58837ca38
allsome <oraclelsy@163.com>
2016-03-08 23:22:23 +0800

    forced to commit

--- LSYPaper/Extension.swift
+++ LSYPaper/Extension.swift
@@ -54,7 +54,7 @@ public extension UIView {
     }
     
     public func addPopSpringAnimation() {
-        self.addSpringAnimation(0.8, durationArray: [0.20,0.275,0.275,0.25], delayArray: [0.0,0.0,0.0,0.0], scaleArray: [1.3,0.9,1.1,1.0])
+        self.addSpringAnimation(0.8, durationArray: [0.20,0.275,0.275,0.25], delayArray: [0.0,0.0,0.0,0.0], scaleArray: [1.4,0.8,1.2,1.0])
     }
     
     public func addFadeAnimation() {
